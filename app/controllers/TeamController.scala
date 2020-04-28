1a5b9c8459cf48df9d7c6d086301113b0029e5d1
Joey Moore <22170468+joeyymoore@users.noreply.github.com>
2020-04-28 16:13:07 +0100

    sorted the routes out for home and team

--- app/controllers/TeamController.scala
+++ app/controllers/TeamController.scala
@@ -2,17 +2,11 @@ package controllers
 
 import javax.inject._
 import play.api.mvc._
-import Models.{Player,Team}
 
 @Singleton
 class TeamController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
 
-  def team() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.team)
+  def team(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.team())
   }
-
-  val Joey: Player = Player("Joey", "Striker")
-
-  val newTeam: Team = Team(List(Joey))
-
 }
