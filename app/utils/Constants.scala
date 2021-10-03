1db82ebc6408ee8f76fbbfc3cdcef1a7eaf3f584
Joey Moore <22170468+joeyymoore@users.noreply.github.com>
2021-10-03 17:40:52 +0100

    added bull/polonia results

--- app/utils/Constants.scala
+++ app/utils/Constants.scala
@@ -18,7 +18,6 @@ object Constants {
   val danSmart: Player = Player(9, Name("Daniel","Smart"), "Midfielder")
   val danSmout: Player = Player(10, Name("Dan","Smout"), "Defender")
   val mattSmart: Player = Player(11, Name("Matt","Smart"), "Midfielder")
-  val kaleem: Player = Player(12, Name("Kaleem","Ramzan"), "Striker")
   val luke: Player = Player(13, Name("Luke","Hester"), "Striker")
   val danGrimbo: Player = Player(14, Name("Dan","Griffiths"), "Defender")
   val charlie: Player = Player(15, Name("Charlie","Bates"), "Defender")
@@ -29,24 +28,29 @@ object Constants {
   val lukeNixon: Player = Player(20, Name("Luke","Nixon"), "Defender")
 
   val fullTeam: Team = Team(List(joey, chad, mattMaiden, adFox, layton, jack, adNixon, danSmout, danSmart,
-    mattSmart, kaleem, danGrimbo, charlie, rich, kieran, cameron, luke, lewis, lukeNixon).sortBy(_.name.lastName))
+    mattSmart, danGrimbo, charlie, rich, kieran, cameron, luke, lewis, lukeNixon).sortBy(_.name.lastName))
 
   //  ******** RESULTS ********
 
-  val phezTeam:Team = Team(List(kieran, jack, adNixon, danGrimbo, lukeNixon, layton, mattMaiden, cameron, joey, kaleem, mattSmart, danSmout, charlie, luke))
-  val phezResult: Result = Result(1, homeTeam = phezTeam, awayTeam = "AFC Phez", goalsFor = 5, goalsAgainst = 2, scorers = List(joey, kaleem, cameron, cameron, adNixon), assists = List(kaleem, kaleem, luke, jack, charlie), date = LocalDate.of(2021, 9, 12))
+//  Other goal and two assists were from Kaleem
+  val phezTeam:Team = Team(List(kieran, jack, adNixon, danGrimbo, lukeNixon, layton, mattMaiden, cameron, joey, mattSmart, danSmout, charlie, luke))
+  val phezResult: Result = Result(1, homeTeam = phezTeam, awayTeam = "AFC Phez", goalsFor = 5, goalsAgainst = 2, scorers = List(joey, cameron, cameron, adNixon), assists = List(luke, jack, charlie), date = LocalDate.of(2021, 9, 12))
 
-  val impactTeam:Team = Team(List(chad, jack, danGrimbo, adNixon, lukeNixon, layton, mattMaiden, cameron, mattSmart, kaleem, joey, kieran, adFox, charlie))
+  val impactTeam:Team = Team(List(chad, jack, danGrimbo, adNixon, lukeNixon, layton, mattMaiden, cameron, mattSmart, joey, kieran, adFox, charlie))
   val impactResult: Result = Result(2, homeTeam = impactTeam, awayTeam = "Impact FC", goalsFor = 1, goalsAgainst = 4, scorers = List(adFox), assists = List(cameron), date = LocalDate.of(2021, 9, 19))
 
-  val fullResults: List[Result] = List(phezResult, impactResult).reverse
+  val bullTeam:Team = Team(List(chad, lukeNixon, adNixon, danGrimbo, danSmout, jack, joey, mattMaiden, mattSmart, cameron, adFox, kieran, layton))
+  val bullResult: Result = Result(3, homeTeam = bullTeam, awayTeam = "Bull in the barne FC", goalsFor = 3, goalsAgainst = 4, scorers = List(adNixon, jack, mattSmart), assists = List(mattSmart, adNixon), date = LocalDate.of(2021, 9, 26))
+
+  val poloniaTeam:Team = Team(List(chad, lukeNixon, layton, danGrimbo, adFox, danSmart, mattMaiden, adNixon, jack, joey, mattSmart, danSmout, lewis, kieran))
+  val poloniaResult: Result = Result(4, homeTeam = poloniaTeam, awayTeam = "Polonia Telford FC", goalsFor = 0, goalsAgainst = 7, scorers = List(), assists = List(), date = LocalDate.of(2021, 10, 3))
+
+  val fullResults: List[Result] = List(phezResult, impactResult, bullResult, poloniaResult).reverse
 
 //  ******** FIXTURES *********
 
-  val bullFixture: Fixture = Fixture(opponent = "BULL IN THE BARNE UNITED F.C.", date = LocalDate.of(2021, 9, 26), venue = "Ketley Bank", gameType = "cup")
-  val poloniaFixture: Fixture = Fixture(opponent = "Polonia Telford FC", date = LocalDate.of(2021, 10, 3), venue = "Mercia Fields", gameType = "league")
   val queensFixture: Fixture = Fixture(opponent = "FC Queens", date = LocalDate.of(2021, 10, 17), venue = "Bowring Park", gameType = "league")
 
-  val fullFixtures: List[Fixture] = List(bullFixture, poloniaFixture, queensFixture)
+  val fullFixtures: List[Fixture] = List(queensFixture)
 
 }
