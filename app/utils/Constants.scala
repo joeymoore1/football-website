9b9e336498ae82ef740a8a4db5fffcbfeeeff812
Joey Moore <joey.moore@capgemini.com>
2022-03-28 18:27:50 +0100

    removing the kierans

--- app/utils/Constants.scala
+++ app/utils/Constants.scala
@@ -21,12 +21,10 @@ object Constants {
   val luke: Player = Player(11, Name("Luke","Hester"), "Striker")
   val danGrimbo: Player = Player(12, Name("Dan","Griffiths"), "Defender")
   val charlie: Player = Player(13, Name("Charlie","Bates"), "Defender")
-  val kieran: Player = Player(14, Name("Kieran","Jakovic"), "Defender")
   val cameron: Player = Player(15, Name("Cameron","Rann"), "Midfielder")
   val lewis: Player = Player(16, Name("Lewis","Evans"), "Defender")
   val lukeNixon: Player = Player(17, Name("Luke","Nixon"), "Defender")
   val rich: Player = Player(18, Name("Rich","Sambrook"), "Midfielder")
-  val kieranW: Player = Player(19, Name("Kieran","Whyman"), "Defender")
   val callum: Player = Player(20, Name("Callum","Stainer"), "Midfielder")
   val kaleem: Player = Player(21, Name("Kaleem","Ramzan"), "Striker")
   val lj: Player = Player(22, Name("Liam","Johnson"), "Striker")
@@ -34,35 +32,35 @@ object Constants {
   val jackWills: Player = Player(24, Name("Jack","Williams"), "Striker")
 
   val fullTeam: Team = Team(List(joey, chad, mattMaiden, adFox, layton, jack, adNixon, danSmout, danSmart,
-    mattSmart, danGrimbo, charlie, kieran, cameron, luke, lewis, lukeNixon, rich, kieranW, callum, kaleem, lj, ben, jackWills).sortBy(_.name.lastName))
+    mattSmart, danGrimbo, charlie, cameron, luke, lewis, lukeNixon, rich, callum, kaleem, lj, ben, jackWills).sortBy(_.name.lastName))
 
   //  ******** RESULTS ********
 
-  val phezTeam:Team = Team(List(kieran, jack, adNixon, danGrimbo, lukeNixon, layton, mattMaiden, cameron, joey, mattSmart, danSmout, charlie, luke, kaleem))
+  val phezTeam:Team = Team(List(jack, adNixon, danGrimbo, lukeNixon, layton, mattMaiden, cameron, joey, mattSmart, danSmout, charlie, luke, kaleem))
   val phezResult: Result = Result(1, homeTeam = phezTeam, awayTeam = "AFC Phez", goalsFor = 5, goalsAgainst = 2, scorers = List(joey, cameron, cameron, adNixon, kaleem), assists = List(luke, jack, charlie, kaleem, kaleem), date = LocalDate.of(2021, 9, 12))
 
-  val impactTeam:Team = Team(List(chad, jack, danGrimbo, adNixon, lukeNixon, layton, mattMaiden, cameron, mattSmart, joey, kieran, adFox, charlie, kaleem))
+  val impactTeam:Team = Team(List(chad, jack, danGrimbo, adNixon, lukeNixon, layton, mattMaiden, cameron, mattSmart, joey, adFox, charlie, kaleem))
   val impactResult: Result = Result(2, homeTeam = impactTeam, awayTeam = "Impact FC", goalsFor = 1, goalsAgainst = 4, scorers = List(adFox), assists = List(cameron), date = LocalDate.of(2021, 9, 19))
 
-  val bullTeam:Team = Team(List(chad, lukeNixon, adNixon, danGrimbo, danSmout, jack, joey, mattMaiden, mattSmart, cameron, adFox, kieran, layton, kaleem))
+  val bullTeam:Team = Team(List(chad, lukeNixon, adNixon, danGrimbo, danSmout, jack, joey, mattMaiden, mattSmart, cameron, adFox, layton, kaleem))
   val bullResult: Result = Result(3, homeTeam = bullTeam, awayTeam = "Bull in the barne FC", goalsFor = 3, goalsAgainst = 4, scorers = List(adNixon, jack, mattSmart), assists = List(mattSmart, adNixon), date = LocalDate.of(2021, 9, 26))
 
-  val poloniaTeam:Team = Team(List(chad, lukeNixon, layton, danGrimbo, adFox, danSmart, mattMaiden, adNixon, jack, joey, mattSmart, danSmout, lewis, kieran))
+  val poloniaTeam:Team = Team(List(chad, lukeNixon, layton, danGrimbo, adFox, danSmart, mattMaiden, adNixon, jack, joey, mattSmart, danSmout, lewis))
   val poloniaResult: Result = Result(4, homeTeam = poloniaTeam, awayTeam = "Polonia Telford FC", goalsFor = 0, goalsAgainst = 6, scorers = List(), assists = List(), date = LocalDate.of(2021, 10, 3))
 
-  val queensTeam:Team = Team(List(chad, kieran, danSmout, adFox, charlie, danSmart, luke, jack, mattSmart, mattMaiden, lewis, layton, danGrimbo))
+  val queensTeam:Team = Team(List(chad, danSmout, adFox, charlie, danSmart, luke, jack, mattSmart, mattMaiden, lewis, layton, danGrimbo))
   val queensResult: Result = Result(5, homeTeam = queensTeam, awayTeam = "FC Queens", goalsFor = 3, goalsAgainst = 1, scorers = List(jack, mattSmart, danSmout), assists = List(jack, mattSmart), date = LocalDate.of(2021, 10, 17))
 
-  val travsTeam:Team = Team(List(chad, layton, lukeNixon, danGrimbo, mattMaiden, kieran, danSmout, mattSmart, jack, rich, luke, lewis, kaleem))
+  val travsTeam:Team = Team(List(chad, layton, lukeNixon, danGrimbo, mattMaiden, danSmout, mattSmart, jack, rich, luke, lewis, kaleem))
   val travsResult: Result = Result(6, homeTeam = travsTeam, awayTeam = "Travellers Joy", goalsFor = 1, goalsAgainst = 1, scorers = List(rich), assists = List(luke), date = LocalDate.of(2021, 10, 24))
 
-  val impact2Team:Team = Team(List(chad, adNixon, lukeNixon, adFox, danSmout, charlie, joey, mattMaiden, kaleem, jack, callum, kieranW, lewis, danGrimbo))
+  val impact2Team:Team = Team(List(chad, adNixon, lukeNixon, adFox, danSmout, charlie, joey, mattMaiden, kaleem, jack, callum, lewis, danGrimbo))
   val impact2Result: Result = Result(7, homeTeam = impact2Team, awayTeam = "Impact FC", goalsFor = 6, goalsAgainst = 2, scorers = List(joey, kaleem, jack, callum, danGrimbo, adNixon), assists = List(kaleem, kaleem, kaleem, joey, joey), date = LocalDate.of(2021, 11, 21))
 
-  val apleyTeam:Team = Team(List(chad, lukeNixon, adFox, danGrimbo, layton, mattMaiden, jack, danSmout, danSmart, adNixon, joey, charlie, luke, kieran))
+  val apleyTeam:Team = Team(List(chad, lukeNixon, adFox, danGrimbo, layton, mattMaiden, jack, danSmout, danSmart, adNixon, joey, charlie, luke))
   val apleyResult: Result = Result(8, homeTeam = apleyTeam, awayTeam = "Apley United FC", goalsFor = 3, goalsAgainst = 5, scorers = List(joey, adNixon, mattMaiden), assists = List(joey, joey), date = LocalDate.of(2021, 11, 28))
 
-  val wrekinTeam:Team = Team(List(chad, layton, lukeNixon, kieran, kieranW, danSmout, danGrimbo, mattMaiden, mattSmart, luke, joey, kaleem, jack, lewis, charlie, callum))
+  val wrekinTeam:Team = Team(List(chad, layton, lukeNixon, danSmout, danGrimbo, mattMaiden, mattSmart, luke, joey, kaleem, jack, lewis, charlie, callum))
   val wrekinResult: Result = Result(9, homeTeam = wrekinTeam, awayTeam = "AFC Wrekin", goalsFor = 0, goalsAgainst = 4, scorers = List(), assists = List(), date = LocalDate.of(2021, 12, 5))
 
   //Other goal was an OG
